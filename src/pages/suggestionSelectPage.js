1b5bca0a4100e55ea68f1f8b14d050c7ce18c313
ahauter <hautera@me.com>
2023-05-01 20:42:51 -0700

    Added a back button

--- src/pages/suggestionSelectPage.js
+++ src/pages/suggestionSelectPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import SuggestionsSelect from '../components/suggestionsSelect';
 import styles from './suggestionSelectPage.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
 
-const SuggestionSelectPage = ({ title, suggestions, selectedSuggestions, onSelect }) => {
+const SuggestionSelectPage = ({ onBack, title, suggestions, selectedSuggestions, onSelect }) => {
     return <>
+        <div
+            class={styles.backButton}
+            onClick={onBack}
+        >
+            <FontAwesomeIcon icon={faArrowLeftLong} /> Back
+        </div>
         <div className={styles.title}>{title}</div>
         <SuggestionsSelect
             suggestions={suggestions}
