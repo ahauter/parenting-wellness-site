31875688a58a6c3db32218a724982ece7e17f2fe
ahauter <hautera@me.com>
2023-05-09 20:22:06 -0700

    Removed back button

--- src/pages/viewSelectionsPage.js
+++ src/pages/viewSelectionsPage.js
@@ -5,7 +5,6 @@ export default function ViewSelectionsPage({ selections, onBack }) {
     return (
         <div>
             <h1>View Selections</h1>
-            <button onClick={onBack}>Back</button>
             <div>
                 <h2>Here are the items you have selected</h2>
                 <ul>
