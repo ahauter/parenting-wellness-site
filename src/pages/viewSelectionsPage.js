3e98a0bf62db1bc2083e079a3ec9c976bef75cd5
ahauter <hautera@me.com>
2023-05-09 20:42:38 -0700

    Add logic to hide selections if nothing selected

--- src/pages/viewSelectionsPage.js
+++ src/pages/viewSelectionsPage.js
@@ -1,19 +1,21 @@
 import React from "react";
 
 export default function ViewSelectionsPage({ selections, onBack }) {
-    console.log(selections)
+    const showSelections = selections && selections.length ? true : false;
     return (
         <div>
             <h1>View Selections</h1>
             <div>
-                <h2>Here are the items you have selected</h2>
+                {showSelections && <h2>Here are the items you have selected</h2>}
                 <ul>
-                    {selections.map((selection) => (
+                    {showSelections && selections.map((selection) => (
                         <li key={selection.id}>
                             {selection}
                         </li>
                     ))}
                 </ul>
+                {!showSelections && <h2>You have not selected any items yet</h2>}
+                {!showSelections && <button onClick={onBack}>Go Back</button>}
             </div>
         </div>
     );
