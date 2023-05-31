d2e70afdc375af16dcd461f7c90a5cc3a963dcf6
ahauter <hautera@me.com>
2023-05-30 18:14:52 -0700

    Played around with making text appear on hover
    No

--- src/components/categorySelect/index.js
+++ src/components/categorySelect/index.js
@@ -9,6 +9,7 @@ export default function CategorySelect({ categories, onSelect, size, getCategory
     };
     size = size || 400;
     // TODO resize text based on size
+    const fontSize = size / 20;
     const numCategories = categories.length;
     // we want to make a selection wheel
     const rotation = 360 / numCategories;
@@ -16,37 +17,58 @@ export default function CategorySelect({ categories, onSelect, size, getCategory
     const vertexOffet = Math.round(r * Math.tan(Math.PI / numCategories));
     const arcPolygon = `polygon(${r - vertexOffet}px 0, ${r + vertexOffet}px 0, 50% 50% )`;
 
+    const hoverEventFactory = (elemId) => {
+        return (isMouseOver) => {
+            const categoryElement = document.getElementById(elemId);
+            categoryElement.style.display = isMouseOver ? 'block' : 'none';
+        };
+    };
 
+    //TODO make category text and icon appear in correct place
     return (
         <div className={styles.categoryContainer}>
             <div className={styles.categoryTitle}>Select a Category</div>
             {categories.map((category, i) => {
-                return <div
-                    key={category}
-                    className={styles.category}
-                    onClick={() => handleSelect(category)}
-                    style={{
-                        height: size,
-                        width: size,
-                        transform: `rotate(${i * rotation}deg)`,
-                        backgroundColor: `${getCategoryColor(category)}`,
-                        clipPath: arcPolygon
-                    }}
-                >
-                    <div style={{ transform: `rotate(${-i * rotation}deg)` }}>
-                        {category}
+                const elemId = `text-display-${category}`;
+                return <>
+                    <div
+                        key={category}
+                        className={styles.category}
+                        onClick={() => handleSelect(category)}
+                        onMouseOver={() => hoverEventFactory(elemId)(true)}
+                        onMouseOut={() => hoverEventFactory(elemId)(false)}
+                        style={{
+                            height: size,
+                            width: size,
+                            backgroundColor: `${getCategoryColor(category)}`,
+                            clipPath: arcPolygon,
+                            transform: `rotate(${i * rotation}deg)`,
+                        }}
+                    />
+                    <div>
+                        <span
+                            id={elemId}
+                            style={{
+                                transform: `rotate(${-i * rotation}deg)`,
+                                display: 'none',
+                                fontSize: fontSize,
+                            }}>
+                            {category}
+                        </span>
                     </div>
-                </div>
+                </>
             })
             }
             <div className={styles.center}
                 style={{
-                    height: size / 2,
-                    width: size / 2,
+                    height: size / 4,
+                    width: size / 4,
                 }}
                 onClick={() => handleSelect(SHOW_ALL_CATEGORIES)}
             >
-                <div className={styles.centerInner}>
+                <div
+                    className={styles.centerInner}
+                    style={{ fontSize: fontSize }}>
                     See all your selections
                 </div>
             </div>
