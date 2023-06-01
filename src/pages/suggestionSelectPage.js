033da86087b53ca32114138608663a31938555e1
ahauter <hautera@me.com>
2023-05-31 17:59:24 -0700

    Added description

--- src/pages/suggestionSelectPage.js
+++ src/pages/suggestionSelectPage.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SuggestionsSelect from '../components/suggestionSelect';
 import styles from './suggestionSelectPage.module.css';
 
-const SuggestionSelectPage = ({ onBack, title, suggestions, selectedSuggestions, onSelect }) => {
+const SuggestionSelectPage = ({ onBack, title, description, suggestions, selectedSuggestions, onSelect }) => {
     return <>
         <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description}</div>
         <SuggestionsSelect
             category={title}
             suggestions={suggestions}
