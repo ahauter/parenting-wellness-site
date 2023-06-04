8bbf8bc408121d0f012c6a9763dee8e0026512ca
ahauter <hauterame.com>
2023-06-03 18:51:24 -0700

    Added logo

--- src/icons.js
+++ src/icons.js
@@ -1,6 +1,7 @@
-import { faAppleWhole } from "@fortawesome/free-solid-svg-icons";
+import { faAppleWhole, faDumbbell } from "@fortawesome/free-solid-svg-icons";
 const iconData = {
     "Proper Nutrition": faAppleWhole,
+    "Engaging in Physical Movement & Getting Outside": faDumbbell,
 };
 export const icons = (categoryName) => {
     const icon = iconData[categoryName];
