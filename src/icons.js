70bae83cd923bd264132e9b63c2ad10920732f3a
ahauter <hauterame.com>
2023-06-10 13:40:49 -0700

    Updated icons

--- src/icons.js
+++ src/icons.js
@@ -1,7 +1,18 @@
-import { faAppleWhole, faDumbbell } from "@fortawesome/free-solid-svg-icons";
+import {
+    faAppleWhole,
+    faMoon,
+    faClock,
+    faPersonWalking,
+    faPeopleGroup,
+    faBrain
+} from "@fortawesome/free-solid-svg-icons";
 const iconData = {
-    "Proper Nutrition": faAppleWhole,
-    "Engaging in Physical Movement & Getting Outside": faDumbbell,
+    "Maintaining Balanced Nutrition": faAppleWhole,
+    "Engaging in Physical Movement & Getting Outside": faPersonWalking,
+    "Having Quality Sleep": faMoon,
+    "Developing Time Management Techniques": faClock,
+    "Practicing Mindfulness": faBrain,
+    "Seeking Social Support": faPeopleGroup
 };
 export const icons = (categoryName) => {
     const icon = iconData[categoryName];
